2dc99894495e180e6e5eabf52ee9c31804ef98b6
Romeo Namunde <romeomureithi@gmail.com>
2020-11-04 20:59:10 +0300

    Setup threading of rock game

---
+++ Rock_Paper_Scissors/client.py
@@ -0,0 +1,22 @@
+import pygame
+from network import network
+import pickle
+pygame.font.init()
+
+width = 700
+height = 700
+win = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Client")
+
+
+
+def redrawWindow(win, game, p):
+    win.fill()
+    pass
+
+
+def main():
+    pass
+
+
+main()
